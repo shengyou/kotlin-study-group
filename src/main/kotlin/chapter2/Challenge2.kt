2d7e08e54f53dcbb86e0467d27edf25deaad5ef2
Shengyou Fan <shengyoufan@gmail.com>
2020-03-17 23:07:40 +0800

    add Challenge The Unicorn's Horn

---
+++ src/main/kotlin/chapter2/Challenge2.kt
@@ -0,0 +1,11 @@
+package chapter2
+
+fun main() {
+    val pubName = "Unicorn's Horn"
+    val publicanName = "Boss"
+    val drinkMenu = listOf("Mead", "Wine", "LaCroix")
+
+    val heroName = "Estragon"
+    var heroHasSteed = false
+    var heroWallet = 50
+}
